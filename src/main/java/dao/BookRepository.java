486e3c4bd6401c7b98bc37ff61ce256037c223a6
kimdongju123 <08555@naver.com>
2024-10-08 13:57:34 +0900

    ppp

---
+++ src/main/java/dao/BookRepository.java
@@ -0,0 +1,62 @@
+package dao;
+
+import java.util.ArrayList;
+import dto.Book;
+
+public class BookRepository {
+	
+	private ArrayList<Book> listOfBooks=new ArrayList<Book>();
+	
+	
+	
+	public BookRepository() {
+		Book book1 = new Book("ISBN1234", "C# 프로그래밍", 27000);
+		  book1.setAuthor("우재남");
+		  book1.setPublisher("한빛아카데미");
+		  book1.setCategory("IT모바일");
+		  book1.setUnitsInStock(1000);
+		  book1.setReleaseDate("2022/10/06");
+		  
+		  Book book2 = new Book("ISBN1235", "자바마스터", 30000);
+		  book2.setAuthor("송미영");
+		  book2.setPublisher("한빛아카데미");
+		  book2.setCategory("IT모바일");
+		  book2.setUnitsInStock(1000);
+		  book2.setReleaseDate("2023/01/01");
+		  
+		  Book book3 = new Book("ISBN1236", "파이", 30000);
+		  book3.setAuthor("최성철");
+		  book3.setPublisher("한빛아카데미");
+		  book3.setCategory("IT모바일");
+		  book3.setUnitsInStock(1000);
+		  book3.setReleaseDate("2023/01/10");
+		
+		  listOfBooks.add(book1);
+		  listOfBooks.add(book2);
+		  listOfBooks.add(book3);
+	}
+	
+	
+		  
+	  public ArrayList<Book> getAllBooks() {
+		  return listOfBooks;
+	  
+}
+	  
+	  public Book getBookById(String bookId) {
+			 Book bookById= null;
+			 
+			 for (int i=0; i < listOfBooks.size(); i++) {
+				 Book book=listOfBooks.get(i);
+				 if (book!= null && book.getBookId()!=null &&
+						 book.getBookId().equals(bookId)) {
+					 bookById=book;
+					 break;
+				 }
+			 }
+			 return bookById;
+				
+		 }
+		 
+
+}
